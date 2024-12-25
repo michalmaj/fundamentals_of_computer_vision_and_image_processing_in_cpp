d3e61861a2e121a4b886d9369470614c2c605d3c
Michał Maj <michalpmaj@gmail.com>
2024-12-25 19:32:03 +0100

    Added Week2: Use Trackbar as Controllers

--- week2/04_adding_a_trackbar_as_controller/01_use_trackbar_as_controllers.cpp
+++ week2/04_adding_a_trackbar_as_controller/01_use_trackbar_as_controllers.cpp
@@ -5,16 +5,29 @@
 
 struct UserData {
     cv::Mat img;
-    std::string windowName{ "Resize Image" };
-    std::string trackbarValue{ "Scale" };
-    std::string trackbarType{ "Type: \n 0: Scale Up \n 1: Scale Down" };
+    std::string window_name{ "Resize Image" };
+    std::string trackbar_value{ "Scale" };
+    std::string trackbar_type{ "Type: \n 0: Scale Up \n 1: Scale Down" };
 
-    int maxScaleUp = 100;
-    int scaleFactor = 1;
-    int scaleType = 0;
-    int maxType = 1;
+    int max_scale_up = 100;
+    int scale_factor = 1;
+    int scale_type = 0;
+    int max_type = 1;
 
 };
+
+void scaleImg(int val, void* data) {
+    UserData* ud = static_cast<UserData*>(data);
+
+    double scaled_factor = 1 + ud->scale_factor / 100.0;
+
+    if (ud->scale_factor == 0) {
+        ud->scale_factor = 1;
+    }
+
+    cv::resize(ud->img, ud->img, cv::Size(), scaled_factor, scaled_factor, cv::INTER_LANCZOS4);
+    cv::imshow(ud->window_name, ud->img);
+}
 
 int main() {
     // Path to an image 
@@ -33,6 +46,18 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    cv::namedWindow(ud.windowName, cv::WINDOW_NORMAL);
+    cv::namedWindow(ud.window_name, cv::WINDOW_NORMAL);
 
+    cv::createTrackbar(ud.trackbar_value, ud.window_name, &ud.scale_factor, ud.max_scale_up, scaleImg, &ud);
+    cv::createTrackbar(ud.trackbar_type, ud.window_name, &ud.scale_factor, ud.max_type, scaleImg, &ud);
+
+    while (true) {
+        cv::imshow(ud.window_name, ud.img);
+        auto key = cv::waitKey(0);
+        if (key == 'q') {
+            break;
+        }
+    }
+
+    cv::destroyWindow(ud.window_name);
 }
