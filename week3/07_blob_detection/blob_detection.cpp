d2ac0fbefc6e21998d817c11c76e9209e6c81662
Michał Maj <michalpmaj@gmail.com>
2025-01-04 14:23:21 +0100

    Added Week3: Blob Detection

--- week3/07_blob_detection/blob_detection.cpp
+++ week3/07_blob_detection/blob_detection.cpp
@@ -1,10 +1,11 @@
+#include <execution>
+#include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/opencv.hpp>
-#include <iostream>
-#include <stdexcept>
-#include <vector>
 #include <random>
 #include <ranges>
+#include <stdexcept>
+#include <vector>
 
 int main() {
     // Path to an image 
@@ -52,8 +53,55 @@ int main() {
         cv::circle(blob, center, radius, color, 2);
         });
 
+    // Setting parameters for SimpleBlobDetector
+    cv::SimpleBlobDetector::Params params;
+
+    // Set threshold
+    params.minThreshold = 10;
+    params.maxThreshold = 200;
+
+    // Filter by area
+    params.filterByArea = true;
+    params.minArea = 150;
+
+    // Filter by circularity
+    params.filterByCircularity = true;
+    params.minCircularity = 0.1;
+
+    // Filter by convexity
+    params.filterByConvexity = true;
+    params.minConvexity = 0.87;
+
+    // Filter by inertia
+    params.filterByInertia = true;
+    params.minInertiaRatio = 0.01;
+
+    detector = cv::SimpleBlobDetector::create(params);
+    detector->detect(img, keypoints);
+
+    // Clone the image
+    auto blob_parametrized{ img.clone() };
+
+    // Convert that image into 3-channel image
+    cv::cvtColor(blob_parametrized, blob_parametrized, cv::COLOR_GRAY2BGR);
+
+    std::ranges::for_each(keypoints, [&blob_parametrized, &dist, &gen](const cv::KeyPoint& key) {
+        // Convert floating-point coordinates to integer
+        cv::Point center{ static_cast<int>(key.pt.x), static_cast<int>(key.pt.y) };
+
+        // Mark the center of the blob in red
+        cv::circle(blob_parametrized, center, 5, cv::Scalar(0, 0, 255), -1);
+
+        // Mark every blob in different color
+        double diameter{ key.size };
+        int radius{ static_cast<int>(diameter / 2) };
+        auto color{ cv::Scalar(dist(gen), dist(gen), dist(gen)) };
+        cv::circle(blob_parametrized, center, radius, color, 2);
+        });
+
     cv::imshow("Original", img);
     cv::imshow("Blobs", blob);
+    cv::imshow("Blobs Parametrized", blob_parametrized);
     cv::waitKey(0);
     cv::destroyAllWindows();
 
