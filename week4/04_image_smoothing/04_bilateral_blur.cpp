d61d32a8259032aecc1e812f557a58054d0d4907
Michał Maj <michalpmaj@gmail.com>
2025-01-17 15:29:43 +0100

    Added Week4: Bilateral Blur

---
+++ week4/04_image_smoothing/04_bilateral_blur.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <stdexcept>
+
+int main() {
+    // Path to an image 
+    std::string path{ "../data/images/gaussian-noise.png" };
+
+    // Load the image
+    cv::Mat img{ cv::imread(path) };
+
+    // Check if the image is loaded correctly
+    try {
+        if (img.empty()) {
+            throw std::runtime_error(std::format("Can't load an image from: {}", path));
+        }
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << '\n';
+        return EXIT_FAILURE;
+    }
+
+    cv::Mat filtered1;
+
+    // Apply bilateral filter with given diameter (non-negative)
+    int diameter{ 15 };
+    int sigma_color{ 80 };
+    int sigma_space{ 80 };
+    cv::bilateralFilter(img, filtered1, diameter, sigma_color, sigma_space);
+
+
+    cv::imshow("Original", img);
+    cv::imshow("With diameter", filtered1);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+
+    return 0;
+}
