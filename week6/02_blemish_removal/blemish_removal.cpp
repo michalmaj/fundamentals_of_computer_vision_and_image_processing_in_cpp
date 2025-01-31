61a6a5a17d742008b326fbd7972c14431561b3b2
Michał Maj <michalpmaj@gmail.com>
2025-01-31 16:06:35 +0100

    Added Week6: Blemish Removal

--- week6/02_blemish_removal/blemish_removal.cpp
+++ week6/02_blemish_removal/blemish_removal.cpp
@@ -9,12 +9,35 @@
 
 class BlemishRemoval {
 public:
-  BlemishRemoval(const std::filesystem::path& path): img_(cv::imread(path.string())) {}
+    BlemishRemoval(const std::filesystem::path& path) : img_(cv::imread(path.string())) {
+        copy_ = img_.clone();
+    }
+
+    // Get images
+    const auto& getImg() const { return img_; }
+    const auto& getMask() const { return mask_; }
+
+    // Get window names
+    const auto& getOriginalWindowName() const { return original_window_name_; }
+
+
+    // Set images
+    void setMask(cv::Mat&& mask) {
+        mask_ = std::move(mask);
+    }
+
+    // Methods
 
 private:
+    // Images
     cv::Mat img_;
-};
+    cv::Mat copy_;
+    cv::Mat mask_;
 
+    // Window names
+    const std::string original_window_name_{ "Original Image" };
+    const std::string mask_window_name{ "Mask" };
+};
 
 
 int main() {
@@ -23,9 +46,22 @@ int main() {
         std::cerr << std::format("Can't load an image from: {}", path.string());
         return EXIT_FAILURE;
     }
-    
 
-    BlemishRemoval br{path};
+    BlemishRemoval br{ path };
 
+    // Create named windows
+    cv::namedWindow(br.getOriginalWindowName(), cv::WINDOW_AUTOSIZE);
+
+    while (true) {
+        auto c = cv::waitKey(0);
+
+        if (c == 'q') {
+            break;
+        }
+
+        cv::imshow(br.getOriginalWindowName(), br.getImg());
+    }
+
+    cv::destroyAllWindows();
     return 0;
 }
